e36ad995d06522f56c80be798d3cb620f54c5c28
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-07 12:18:30 +0900

    class

---
+++ python-basic/class.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+kirishima = Person("Kirishima", 15)
+
+print(kirishima.name)
+print(kirishima.age)
